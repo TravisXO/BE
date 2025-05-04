a1c5698f4fbcd7ac9066b2a90b0c35f6290699fa
Shamil Mondoka <unknxwntransmission@gmail.com>
2025-05-04 13:06:34 +0300

    Manage Identity View Finilized

--- Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -65,6 +65,12 @@ namespace  BE.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
+        /// 
+        public static string SetPassword => "SetPassword";
+
+        public static string SetPasswordNavClass(ViewContext viewContext) =>
+            PageNavClass(viewContext, SetPassword);
+
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
         /// <summary>
