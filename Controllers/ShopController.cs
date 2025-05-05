af363013982b36f90c4ad3b5bdc330edd4385df1
Shamil Mondoka <unknxwntransmission@gmail.com>
2025-05-05 15:23:34 +0300

    Pagination + Product Grid Population

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -6,15 +6,28 @@ namespace BE.Controllers
 {
     public class ShopController : Controller
     {
-        public IActionResult Shop()
+        public IActionResult Shop(int page = 1)
         {
             var jsonPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/products/bags-backpacks/bags-&-backpacks-list.json");
             if (!System.IO.File.Exists(jsonPath))
                 return NotFound("Product data file not found.");
 
             var json = System.IO.File.ReadAllText(jsonPath);
-            var products = JsonSerializer.Deserialize<List<ProductList>>(json);
-            return View(products);
+            var allProducts = JsonSerializer.Deserialize<List<ProductList>>(json);
+
+            int itemsPerPage = 20;
+            int totalItems = allProducts.Count;
+            int totalPages = (int)Math.Ceiling(totalItems / (double)itemsPerPage);
+
+            var pagedProducts = allProducts
+                .Skip((page - 1) * itemsPerPage)
+                .Take(itemsPerPage)
+                .ToList();
+
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = totalPages;
+
+            return View(pagedProducts);
         }
     }
 }
